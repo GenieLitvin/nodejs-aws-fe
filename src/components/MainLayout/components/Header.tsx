0f6e21adb9622e9213f03a255b52860eb2b3e224
Eugeniya Litvin <genie.litvin@gmail.com>
2024-06-20 12:52:54 +0200

    header

--- src/components/MainLayout/components/Header.tsx
+++ src/components/MainLayout/components/Header.tsx
@@ -46,7 +46,7 @@ export default function Header() {
     <AppBar position="relative">
       <Toolbar>
         <Typography variant="h6" className={classes.title}>
-          <Link className={classes.homeLink} to="/">My OWN Store!</Link>
+          <Link className={classes.homeLink} to="/">Board Game Store</Link>
         </Typography>
 
         {auth && (
