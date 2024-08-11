900e21337c0988787e99547cc60a358522c9a35f
Eugeniya Litvin <genie.litvin@gmail.com>
2024-08-11 15:11:35 +0200

    task-10

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,15 +1,10 @@
 
-const be_path = 'https://5gwtxwth8l.execute-api.eu-west-1.amazonaws.com/prod';
-const cart_path = `https://bv8wa3591m.execute-api.eu-west-1.amazonaws.com/prod`;
-//'http://localhost:3001'
-
 const API_PATHS = {
-  product: be_path,
-  order:  cart_path,
-  import:  `https://92mkrazgkg.execute-api.eu-west-1.amazonaws.com/prod`,
-  bff: be_path,
-  cart: 'http://genielitvin-cart-api-dev4.eu-west-1.elasticbeanstalk.com/' ,
-
+  product:'http://genielitvin-bff-api-dev.eu-west-1.elasticbeanstalk.com',
+  order:  `https://bv8wa3591m.execute-api.eu-west-1.amazonaws.com/prod`,
+  import: `https://92mkrazgkg.execute-api.eu-west-1.amazonaws.com/prod`,
+  bff:    'http://genielitvin-bff-api-dev.eu-west-1.elasticbeanstalk.com',
+  cart:   'http://genielitvin-bff-api-dev.eu-west-1.elasticbeanstalk.com/' ,
 };
-/// 
+
 export default API_PATHS;
